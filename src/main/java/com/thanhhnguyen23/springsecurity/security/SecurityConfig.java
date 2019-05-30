9e39e3ef6bdff771953e942e240ebea33e03f32f
Thanh Nguyen <thanhhnguyen23@gmail.com>
2019-05-30 12:59:47 -0400

    found alternative to using NoOpPasswordEncoder; implemented working password encoder via BCryptPasswordEncoder; cleaning up unnecessary from deprecated class

--- src/main/java/com/thanhhnguyen23/springsecurity/security/SecurityConfig.java
+++ src/main/java/com/thanhhnguyen23/springsecurity/security/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.thanhhnguyen23.springsecurity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 /*
@@ -19,25 +22,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	// authentication : User --> Roles
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.inMemoryAuthentication()
-		.passwordEncoder(org.springframework
-				.security
-				.crypto
-				.password
-				/*
-				 * NoOpPasswordEncoder is deprecated
-				 * 		> This PasswordEncoder is not secure. 
-				 * 		> Instead use an adaptive one way function like 
-				 * 			* BCryptPasswordEncoder, 
-				 * 			* Pbkdf2PasswordEncoder, or 
-				 * 			* SCryptPasswordEncoder. 
-				 * 		> Even better use DelegatingPasswordEncoder which supports password upgrades.
-				 */
-				//TODO - find alternative since NoOpPasswordEncoder is deprecated
-				.NoOpPasswordEncoder
-				.getInstance())
-		.withUser("user1").password("secret1").roles("USER")
+//		.passwordEncoder(org.springframework
+//				.security
+//				.crypto
+//				.password
+//				/*
+//				 * NoOpPasswordEncoder is deprecated
+//				 * 		> This PasswordEncoder is not secure. 
+//				 * 		> Instead use an adaptive one way function like 
+//				 * 			* BCryptPasswordEncoder, 
+//				 * 			* Pbkdf2PasswordEncoder, or 
+//				 * 			* SCryptPasswordEncoder. 
+//				 * 		> Even better use DelegatingPasswordEncoder which supports password upgrades.
+//				 */
+//				//TODO - find alternative since NoOpPasswordEncoder is deprecated
+//				.NoOpPasswordEncoder
+//				.getInstance())
+		.withUser("user1").password(
+//				"secret1")
+				passwordEncoder().encode("secret1"))
+		.roles("USER")
 		.and()
-		.withUser("admin1").password("secret1").roles("USER", "ADMIN");
+		.withUser("admin1").password(
+//				"secret1")
+				passwordEncoder().encode("secret1"))
+		.roles("USER", "ADMIN");
 	}
 	// authorization : Role --> Access
 	/*
@@ -50,6 +59,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	}
 	
-
-
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
